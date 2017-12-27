eb7520c6ad930beab4e89165fbb30bdaa59e8e97
Bruno Carlin <bruno.carlin@waarp.org>
2017-12-27 18:56:58 +0100

    update internal dependencies

--- src/main/java/org/waarp/thrift/util/Version.java
+++ src/main/java/org/waarp/thrift/util/Version.java
@@ -3,7 +3,7 @@ package org.waarp.thrift.util;
 /** Provides the version information of Waarp Thrift. */
 public final class Version {
  /** The version identifier. */
- public static final String ID = "3.0.3";
+ public static final String ID = "3.0.4";
  /** Prints out the version identifier to stdout. */
  public static void main(String[] args) { System.out.println(ID); }
  private Version() { super(); }
