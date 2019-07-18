5a3807345a8d4c98fbf32fca7ae95dfa2a716ce7
Frederic Bregier <frederic.bregier@waarp.fr>
2019-07-18 11:25:58 +0200

    Add Junit to v3.1.0

--- src/test/java/org/waarp/thrift/r66/ClientExample.java
+++ src/test/java/org/waarp/thrift/r66/ClientExample.java
@@ -1,20 +1,22 @@
-/**
- * This file is part of Waarp Project.
- * 
- * Copyright 2009, Frederic Bregier, and individual contributors by the @author tags. See the
- * COPYRIGHT.txt in the distribution for a full listing of individual contributors.
- * 
- * All Waarp Project is free software: you can redistribute it and/or modify it under the terms of
- * the GNU General Public License as published by the Free Software Foundation, either version 3 of
- * the License, or (at your option) any later version.
- * 
- * Waarp is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even
- * the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
- * Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License along with Waarp . If not, see
- * <http://www.gnu.org/licenses/>.
- */
+/*******************************************************************************
+ * This file is part of Waarp Project (named also Waarp or GG).
+ *
+ *  Copyright (c) 2019, Waarp SAS, and individual contributors by the @author
+ *  tags. See the COPYRIGHT.txt in the distribution for a full listing of
+ *  individual contributors.
+ *
+ *  All Waarp Project is free software: you can redistribute it and/or
+ *  modify it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or (at your
+ *  option) any later version.
+ *
+ *  Waarp is distributed in the hope that it will be useful, but WITHOUT ANY
+ *  WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ *  A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License along with
+ *  Waarp . If not, see <http://www.gnu.org/licenses/>.
+ ******************************************************************************/
 package org.waarp.thrift.r66;
 
 import org.apache.thrift.TException;
@@ -23,106 +25,107 @@ import org.apache.thrift.protocol.TProtocol;
 import org.apache.thrift.transport.TSocket;
 import org.apache.thrift.transport.TTransport;
 import org.apache.thrift.transport.TTransportException;
-import org.waarp.thrift.r66.Action;
-import org.waarp.thrift.r66.R66Request;
-import org.waarp.thrift.r66.R66Result;
-import org.waarp.thrift.r66.R66Service;
-import org.waarp.thrift.r66.RequestMode;
 
 /**
  * Example of Java class to interact with the Thrift R66 service
- * 
+ *
  * @author "Frederic Bregier"
- * 
  */
 public class ClientExample implements Runnable {
-    private static final int PORT = 7911;
-    private static final int tries = 100000;
+  private static final int PORT = 7911;
+  private static final int tries = 100000;
 
-    public static void main(String[] args) {
-        try {
-            TTransport transport = null;
-            transport = new TSocket("localhost", PORT);
-            TProtocol protocol = new TBinaryProtocol(transport);
-            R66Service.Client client = new R66Service.Client(protocol);
-            transport.open();
-            R66Request request = new R66Request(RequestMode.SYNCTRANSFER);
-            request.setFromuid("myclient");
-            request.setDestuid("mypartner");
-            request.setRule("myruletouse");
-            request.setFile("pathtomyfile.txt");
-            request.setInfo("my info send on the wire");
-            request.setMd5(true);
+  public static void main(String[] args) {
+    new ClientExample().testClient();
+  }
 
-            System.out.println("REQUEST1: " + request.toString());
-            R66Result result = client.transferRequestQuery(request);
-            System.out.println("RESULT1: " + result.toString());
+  public void testClient() {
+    try {
+      TTransport transport = null;
+      transport = new TSocket("localhost", PORT);
+      TProtocol protocol = new TBinaryProtocol(transport);
+      R66Service.Client client = new R66Service.Client(protocol);
+      transport.open();
+      R66Request request = new R66Request(RequestMode.SYNCTRANSFER);
+      request.setFromuid("myclient");
+      request.setDestuid("mypartner");
+      request.setRule("myruletouse");
+      request.setFile("pathtomyfile.txt");
+      request.setInfo("my info send on the wire");
+      request.setMd5(true);
 
-            long start = System.currentTimeMillis();
-            for (int i = 0; i < tries; i++) {
-                result = client.transferRequestQuery(request);
-            }
-            long end = System.currentTimeMillis();
-            System.out
-                    .println("Delay: " + (end - start) + " : " + ((tries * 1000) / (end - start)));
+      System.out.println("REQUEST1: " + request.toString());
+      R66Result result = client.transferRequestQuery(request);
+      System.out.println("RESULT1: " + result.toString());
 
-            request.setMode(RequestMode.INFOREQUEST);
-            request.setTid(result.getTid());
-            request.setAction(Action.Detail);
-            result = client.infoTransferQuery(request);
-            System.out.println("RESULT2: " + result.toString());
+      long start = System.currentTimeMillis();
+      for (int i = 0; i < tries; i++) {
+        result = client.transferRequestQuery(request);
+      }
+      long end = System.currentTimeMillis();
+      System.out
+          .println("Delay: " + (end - start) + " : " +
+                   ((tries * 1000) / (end - start)));
 
-            System.out.println("Exist: "
-                    +
-                    client.isStillRunning(request.getFromuid(), request.getDestuid(),
-                            request.getTid()));
+      request.setMode(RequestMode.INFOREQUEST);
+      request.setTid(result.getTid());
+      request.setAction(Action.Detail);
+      result = client.infoTransferQuery(request);
+      System.out.println("RESULT2: " + result.toString());
 
-            request.setMode(RequestMode.INFOFILE);
-            request.setAction(Action.List);
-            result = client.infoTransferQuery(request);
-            System.out.println("RESULT3: " + result.toString());
+      System.out.println("Exist: "
+                         +
+                         client.isStillRunning(request.getFromuid(),
+                                               request.getDestuid(),
+                                               request.getTid()));
 
-            transport.close();
-        } catch (TTransportException e) {
-            e.printStackTrace();
-        } catch (TException e) {
-            e.printStackTrace();
-        }
+      request.setMode(RequestMode.INFOFILE);
+      request.setAction(Action.List);
+      result = client.infoTransferQuery(request);
+      System.out.println("RESULT3: " + result.toString());
+
+      transport.close();
+    } catch (TTransportException e) {
+      e.printStackTrace();
+    } catch (TException e) {
+      e.printStackTrace();
     }
+  }
 
-    public void run() {
-        try {
-            TTransport transport = null;
-            transport = new TSocket("localhost", PORT);
-            TProtocol protocol = new TBinaryProtocol(transport);
-            R66Service.Client client = new R66Service.Client(protocol);
-            transport.open();
-            R66Request request = new R66Request(RequestMode.SYNCTRANSFER);
-            request.setFromuid("myclient");
-            request.setDestuid("mypartner");
-            request.setRule("myruletouse");
-            request.setFile("pathtomyfile.txt");
-            request.setInfo("my info send on the wire");
-            request.setMd5(true);
+  public void run() {
+    try {
+      TTransport transport = null;
+      transport = new TSocket("localhost", PORT);
+      TProtocol protocol = new TBinaryProtocol(transport);
+      R66Service.Client client = new R66Service.Client(protocol);
+      transport.open();
+      R66Request request = new R66Request(RequestMode.SYNCTRANSFER);
+      request.setFromuid("myclient");
+      request.setDestuid("mypartner");
+      request.setRule("myruletouse");
+      request.setFile("pathtomyfile.txt");
+      request.setInfo("my info send on the wire");
+      request.setMd5(true);
 
-            System.out.println("REQUEST1: " + request.toString());
-            R66Result result = client.transferRequestQuery(request);
-            System.out.println("RESULT1: " + result.toString());
+      System.out.println("REQUEST1: " + request.toString());
+      R66Result result = client.transferRequestQuery(request);
+      System.out.println("RESULT1: " + result.toString());
 
-            long start = System.currentTimeMillis();
-            for (int i = 0; i < tries; i++) {
-                result = client.transferRequestQuery(request);
-            }
-            long end = System.currentTimeMillis();
-            System.out
-                    .println("Delay: " + (end - start) + " : " + ((tries * 1000) / (end - start)));
+      long start = System.currentTimeMillis();
+      for (int i = 0; i < tries; i++) {
+        result = client.transferRequestQuery(request);
+      }
+      long end = System.currentTimeMillis();
+      System.out
+          .println("Delay: " + (end - start) + " : " +
+                   ((tries * 1000) / (end - start)));
 
-            transport.close();
-        } catch (TTransportException e) {
-            e.printStackTrace();
-        } catch (TException e) {
-            e.printStackTrace();
-        }
+      transport.close();
+    } catch (TTransportException e) {
+      e.printStackTrace();
+    } catch (TException e) {
+      e.printStackTrace();
     }
+  }
 
 }
